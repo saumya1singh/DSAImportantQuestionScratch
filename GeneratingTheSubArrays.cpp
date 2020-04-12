7f11336db74e77575b98992df0d36f1f2e64ccaf
Saumya <singhsaumyas150@gmail.com>
2020-04-12 21:42:42 +0530

    Finding subarrays

---
+++ GeneratingTheSubArrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //code to find the subArray of an array
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n;++i){
+        for(int j=i;j<n;++j){
+            for(int k=i;k<=j;++k){
+                cout<< a[k] << " ";
+            }
+            cout<<endl;
+        }
+    }
+}
